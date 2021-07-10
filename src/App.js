af12c653272f16fb32e0a1b2234d7875f2f751cd
leeyunsik <leeys9932@naver.com>
2021-07-10 20:36:15 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
+import { func } from "prop-types";
 import React from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: [{}, {}],
-    };
-    getMovies = async () => {
-        //비동기
-        console.log("3");
-        const {
-            data: {
-                data: { movies },
-            },
-        } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"); //axops 는 그렇게 빠르지 않으므로 componentDidMount함수가 끝날때까지 우리가 기다려야한다
-        console.log("4");
-
-        this.setState({ movies, isLoading: false }); //setState를 호출하면 render 다시실행
-        console.log("5");
-    };
-    componentDidMount() {
-        console.log("2");
-        this.getMovies();
-    }
-    render() {
-        console.log("1");
-        const { isLoading, movies } = this.state;
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader_text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map((movie) => (
-                            <Movie
-                                key={movie.id}
-                                id={movie.id}
-                                year={movie.year}
-                                title={movie.title}
-                                summary={movie.summary}
-                                poster={movie.medium_cover_image}
-                                genres={movie.genres}
-                            />
-                        ))}
-                    </div>
-                )}
-            </section>
-        ); //map:배열로 다시 가져와주는것
-    }
+function App() {
+    return (
+        <HashRouter>
+            <Route path="/about" component={About} />
+        </HashRouter>
+    );
 }
+
 export default App;
 
 // class App extends React.Component {
---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "..components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [{}, {}],
+    };
+    getMovies = async () => {
+        //비동기
+        console.log("3");
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"); //axops 는 그렇게 빠르지 않으므로 componentDidMount함수가 끝날때까지 우리가 기다려야한다
+        console.log("4");
+
+        this.setState({ movies, isLoading: false }); //setState를 호출하면 render 다시실행
+        console.log("5");
+    };
+    componentDidMount() {
+        console.log("2");
+        this.getMovies();
+    }
+    render() {
+        console.log("1");
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader_text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map((movie) => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
+        ); //map:배열로 다시 가져와주는것
+    }
+}
+export default App;
